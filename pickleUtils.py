a6fb50d3dc57dc1e61a5b4d239e06cf5ebe489f7
aalexx-S <alex530519@gmail.com>
2017-05-17 20:49:09 +0800

    Add pickleUtils.
    Add pickleUtils. A simple facade for package 'pickle'.

---
+++ pickleUtils.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+import os
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+class PickleUtils:
+    """
+    Provide a simple facade for the python package 'pickle'.
+    A file name for reading and writing is needed, and objects will be read from and writen to the file given.
+    The file and directories will be created if not exists.
+
+    Attributes:
+        PICKLE_NAME: the name of the file writing to and reading from.
+    """
+    PICKLE_NAME = ""
+
+    def __init__(self, filename):
+        """
+        Init PickleUtils with a file name.
+
+        Args:
+            filename:the full name to the file.
+        """
+        self.PICKLE_NAME = filename
+
+    def pickle_check(self):
+        """
+        Check if the file exists.
+
+        Returns:
+            Return True if the file exists, otherwise false.
+        """
+        my_pickle_file = Path(self.PICKLE_NAME)
+        if my_pickle_file.is_file():
+            return True
+        return False
+
+    def pickle_create_dir(self):
+        """
+        Create the directory for the file (not including the file itself.)
+        """
+        directory = os.path.dirname(self.PICKLE_NAME)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def pickle_read(self):
+        """
+        Read objects from the file. Objects will be returned in a list.
+        The order of objects will be the same as the order given when writing.
+
+        Returns:
+            A list of objects.
+
+            examples:
+                ['Test', {'a':1, 'b':2}]
+        """
+        items = []
+        with open(self.PICKLE_NAME, 'rb') as pf:
+            while True:
+                try:
+                    items.append(pickle.load(pf))
+                except EOFError:
+                    break
+        return items
+
+    def pickle_write(self, list_of_objects):
+        """
+        Write a list of objects to the file. Any content in the file will be cleared before writing.
+
+        Args:
+            list_of_objects:a list of objects to be written to the file.
+
+            examples:
+                ['test', {'a':1, 'b':2}]
+        """
+        self.pickle_create_dir()
+        with open(self.PICKLE_NAME, 'wb') as pf:
+            for item in list_of_objects:
+                pickle.dump(item, pf)
